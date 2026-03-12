19888edcc399a1527e7b6248e63023e2bcc36eb0
Paras <jhaparas13@gmail.com>
2026-03-13 00:18:29 +0530

    Initial SpringBoot Project Setup

---
+++ src/test/java/com/paras/CacheCart/CacheCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paras.CacheCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
