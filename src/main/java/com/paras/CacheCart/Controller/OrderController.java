e3b91b9d5d51edbae19cf64c8e92809289bf7f1d
Paras <jhaparas13@gmail.com>
2026-03-29 00:11:10 +0530

    Updated Order Controller with API Wrapper DTO

--- src/main/java/com/paras/CacheCart/Controller/OrderController.java
+++ src/main/java/com/paras/CacheCart/Controller/OrderController.java
@@ -1,14 +1,19 @@
 package com.paras.CacheCart.Controller;
 
+import com.paras.CacheCart.DTO.ApiResponse;
 import com.paras.CacheCart.DTO.OrderRequest;
 import com.paras.CacheCart.Entity.Order;
 import com.paras.CacheCart.Service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +22,14 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public Order placeOrder(@Valid @RequestBody OrderRequest request) {
-        return orderService.placeOrder(request);
+    public ResponseEntity<ApiResponse<Order>> placeOrder(@Valid @RequestBody OrderRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.<Order>builder()
+                        .success(true)
+                        .message("Order Created Successfully")
+                        .data(orderService.placeOrder(request))
+                        .timeStamp(LocalDateTime.now())
+                        .build()
+        );
     }
 }
