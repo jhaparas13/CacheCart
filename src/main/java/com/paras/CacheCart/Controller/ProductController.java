30471569c8fceaf46730187ade7213dd9e7695c0
Paras <jhaparas13@gmail.com>
2026-03-28 23:20:56 +0530

    Updated Controllers with Wrapper DTO

--- src/main/java/com/paras/CacheCart/Controller/ProductController.java
+++ src/main/java/com/paras/CacheCart/Controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.paras.CacheCart.Controller;
 
+import com.paras.CacheCart.DTO.ApiResponse;
 import com.paras.CacheCart.DTO.ProductResponse;
 import com.paras.CacheCart.Entity.Product;
 import com.paras.CacheCart.Service.ProductService;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -18,23 +20,51 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
+    public ResponseEntity<ApiResponse<Product>> createProduct(@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.<Product>builder()
+                        .success(true)
+                        .message("Product Successfully Created")
+                        .data(productService.createProduct(product))
+                        .timeStamp(LocalDateTime.now())
+                        .build()
+        );
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok(productService.getProductById(id));
+    public ResponseEntity<ApiResponse<ProductResponse>> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(
+                ApiResponse.<ProductResponse>builder()
+                        .success(true)
+                        .message("Product fetched Successfully")
+                        .data(productService.getProductById(id))
+                        .timeStamp(LocalDateTime.now())
+                        .build()
+        );
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponse>> getAllProduct() {
-        return ResponseEntity.ok(productService.getAllProducts());
+    public ResponseEntity<ApiResponse<List<ProductResponse>>> getAllProduct() {
+        return ResponseEntity.ok(
+                ApiResponse.<List<ProductResponse>>builder()
+                        .success(true)
+                        .message("All products fetched successfully")
+                        .data(productService.getAllProducts())
+                        .timeStamp(LocalDateTime.now())
+                        .build()
+        );
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProductResponse> updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        return ResponseEntity.ok(productService.updateProduct(id, product));
+    public ResponseEntity<ApiResponse<ProductResponse>> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return ResponseEntity.ok(
+                ApiResponse.<ProductResponse>builder()
+                        .success(true)
+                        .message("Product Updated")
+                        .data(productService.updateProduct(id, product))
+                        .timeStamp(LocalDateTime.now())
+                        .build()
+        );
     }
 
     @DeleteMapping("/{id}")
